38038b92416e96741ab29d307e2bdbeff97ce1ac
jermaine joseph <105750913+Jermaine1988@users.noreply.github.com>
2022-08-04 16:27:32 -0400

    Add files via upload

---
+++ Classes/Employee.js
@@ -0,0 +1,18 @@
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName () {
+        return this.name;
+    }
+    getName () {
+        return this.id;
+    }
+    getName () {
+        return this.email;
+    }
+}
+module.exports = Employee;
